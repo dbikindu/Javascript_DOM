0f0d3a61100de27fbb8c800e829985393404300c
dbikindu <dbmakanka@gmail.com>
2018-12-15 18:08:39 -0500

    Added filter code

--- static/js/app.js
+++ static/js/app.js
@@ -10,12 +10,29 @@ console.log(tableData);
 // Use Arrow function to append data to the table
 data.forEach((ufo) => {
   var row = tbody.append("tr");
-  Object.entries(ufo).forEach(([key, value]) => {
-    var cell = tbody.append("td");
+  Object.values(ufo).forEach((value) => {
+    var cell = row.append("td");
     cell.text(value);
   });
 });
 
-// Set filteredData to dataSet initially
-var filteredData = {};
+// Set filteredData to select filter button 
+var filteredData = d3.select("#filter-btn");
 
+filteredData.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  
+  var filteredData = tableData.filter(row => row.datetime === inputValue);
+
+  console.log(filteredData);
+});
